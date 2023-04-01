bb395f38be3f4c2a75d89c5601247b57ea2202af
Leandro Quipildor <83482016+LeooQuipildor@users.noreply.github.com>
2023-04-01 16:11:45 -0300

    Agregue toda la logica con sus respectivos buttons funcionales, faltaria hacer el sitio responsive y pulir algunos detaller

--- js/app.js
+++ js/app.js
@@ -11,39 +11,41 @@ class Persona {
 
     mostrarGeneracion(){
         if (this.anioNacimiento >= 1930 && this.anioNacimiento <= 1948){
-            document.write(`<br> ${this.nombre} pertence a la SILENT GENERATION y su rasgo caracteristico es la AUSTERIDAD <br>`);
+            alert(`${this.nombre} pertence a la SILENT GENERATION y su rasgo caracteristico es la AUSTERIDAD`);
         }
         if (this.anioNacimiento >= 1949 && this.anioNacimiento <= 1968){
-            document.write(`<br> ${this.nombre} pertence a la BABY BOOM y su rasgo caracteristico es la AMBICION <br>`);
+            alert(`${this.nombre} pertence a la BABY BOOM y su rasgo caracteristico es la AMBICION`);
         }
         if (this.anioNacimiento >= 1969 && this.anioNacimiento <= 1980){
-            document.write(`<br> ${this.nombre} pertence a la GENERACION X y su rasgo caracteristico es la OBSESION POR EL EXITO <br>`);
+            alert(`${this.nombre} pertence a la GENERACION X y su rasgo caracteristico es la OBSESION POR EL EXITO`);
         }
         if (this.anioNacimiento >= 1981 && this.anioNacimiento <= 1993){
-            document.write(`<br> ${this.nombre} pertence a la GENERACION Y(millennials) y su rasgo caracteristico es la FRUSTRACION <br>`);
+            alert(`${this.nombre} pertence a la GENERACION Y(millennials) y su rasgo caracteristico es la FRUSTRACION`);
         }
         if (this.anioNacimiento >= 1994 && this.anioNacimiento <= 2010){
-            document.write(`<br> ${this.nombre} pertence a la GENERACION Z y su rasgo caracteristico es la IRREVERENCIA <br>`);
+            alert(`${this.nombre} pertence a la GENERACION Z y su rasgo caracteristico es la IRREVERENCIA`);
         }
     }
 
     esMayorDeEdad(){
         if (this.edad>=18){
-            document.write(`<br>${this.nombre} es MAYOR de edad. <br>`);
+            alert(`${this.nombre} es MAYOR de edad.`);
         }
         else{
-            document.write(`<br>${this.nombre} es MENOR de edad. <br>`)
+            alert(`${this.nombre} es MENOR de edad.`)
         }
     }
 
     mostrarDatos(){
-        document.write(`<br>Nombre: ${this.nombre}<br>`);
-        document.write(`Edad: ${this.edad}<br>`);
-        document.write(`DNI: ${this.dni}<br>`);
-        document.write(`Sexo: ${this.sexo}<br>`);
-        document.write(`Peso: ${this.peso}<br>`);
-        document.write(`Altura: ${this.altura}<br>`);
-        document.write(`Año de nacimiento: ${this.anioNacimiento}<br>`);
+        alert(`>
+        Nombre: ${this.nombre}
+        Edad: ${this.edad}
+        DNI: ${this.dni}
+        Sexo: ${this.sexo}
+        Peso: ${this.peso}
+        Altura: ${this.altura}
+        Año de nacimiento: ${this.anioNacimiento}
+        `)
     }
 
     generarDNI(){
@@ -51,8 +53,41 @@ class Persona {
     }
 }
 
-console.log(Persona)
 
-let persona1 = new Persona("Leandro", 21, "Masculino", 65.7, "1.75m", 2001 );
+let formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", enviarDatos);
 
-let persona2 = new Persona("Lionel Messi", 35, "Masculino", 67.3, "1.70m", 1987);
+function enviarDatos(e){
+    e.preventDefault();
+    console.log(`enviando`)
+    let nombre = document.getElementById("nombre").value;
+    let edad = document.getElementById("edad").value;
+    let sexo = document.getElementById("sexo").value;
+    let peso = document.getElementById("peso").value;
+    let altura = document.getElementById("altura").value;
+    let anio = document.getElementById("anio").value;
+
+    nuevaPersona = new Persona(nombre, edad, sexo, peso, altura, anio);
+    nuevaPersona.generarDNI();
+}
+
+let btnMostrarInfo = document.getElementById("mostrarInfo");
+btnMostrarInfo.addEventListener("click", mostrarInfo);
+
+function mostrarInfo() {
+    nuevaPersona.mostrarDatos();
+}
+
+let btnMostrarGen = document.getElementById("mostrarGen");
+btnMostrarGen.addEventListener("click", mostrarGen);
+
+function mostrarGen(){
+    nuevaPersona.mostrarGeneracion();
+}
+
+let btnMayorEdad = document.getElementById("mayor");
+btnMayorEdad.addEventListener("click", mayor);
+
+function mayor(){
+    nuevaPersona.esMayorDeEdad();
+}
