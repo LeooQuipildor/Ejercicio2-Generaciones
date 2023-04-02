c503f8fe5eff28856da4e8257bd7bdf27107a83f
Leandro Quipildor <83482016+LeooQuipildor@users.noreply.github.com>
2023-04-02 20:26:26 -0300

    Corregi un problema con los buttons del form y les puse un poco de estilo

--- js/app.js
+++ js/app.js
@@ -1,43 +1,52 @@
 class Persona {
-    constructor(nombre, edad, sexo, peso, altura, anioNacimiento) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.dni = "";
-        this.sexo = sexo;
-        this.peso = peso;
-        this.altura = altura;
-        this.anioNacimiento = anioNacimiento;
-    }
+  constructor(nombre, edad, sexo, peso, altura, anioNacimiento) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.dni = "";
+    this.sexo = sexo;
+    this.peso = peso;
+    this.altura = altura;
+    this.anioNacimiento = anioNacimiento;
+  }
 
-    mostrarGeneracion(){
-        if (this.anioNacimiento >= 1930 && this.anioNacimiento <= 1948){
-            alert(`${this.nombre} pertence a la SILENT GENERATION y su rasgo caracteristico es la AUSTERIDAD`);
-        }
-        if (this.anioNacimiento >= 1949 && this.anioNacimiento <= 1968){
-            alert(`${this.nombre} pertence a la BABY BOOM y su rasgo caracteristico es la AMBICION`);
-        }
-        if (this.anioNacimiento >= 1969 && this.anioNacimiento <= 1980){
-            alert(`${this.nombre} pertence a la GENERACION X y su rasgo caracteristico es la OBSESION POR EL EXITO`);
-        }
-        if (this.anioNacimiento >= 1981 && this.anioNacimiento <= 1993){
-            alert(`${this.nombre} pertence a la GENERACION Y(millennials) y su rasgo caracteristico es la FRUSTRACION`);
-        }
-        if (this.anioNacimiento >= 1994 && this.anioNacimiento <= 2010){
-            alert(`${this.nombre} pertence a la GENERACION Z y su rasgo caracteristico es la IRREVERENCIA`);
-        }
+  mostrarGeneracion() {
+    if (this.anioNacimiento >= 1930 && this.anioNacimiento <= 1948) {
+      alert(
+        `${this.nombre} pertence a la SILENT GENERATION y su rasgo caracteristico es la AUSTERIDAD`
+      );
     }
-
-    esMayorDeEdad(){
-        if (this.edad>=18){
-            alert(`${this.nombre} es MAYOR de edad.`);
-        }
-        else{
-            alert(`${this.nombre} es MENOR de edad.`)
-        }
+    if (this.anioNacimiento >= 1949 && this.anioNacimiento <= 1968) {
+      alert(
+        `${this.nombre} pertence a la BABY BOOM y su rasgo caracteristico es la AMBICION`
+      );
     }
+    if (this.anioNacimiento >= 1969 && this.anioNacimiento <= 1980) {
+      alert(
+        `${this.nombre} pertence a la GENERACION X y su rasgo caracteristico es la OBSESION POR EL EXITO`
+      );
+    }
+    if (this.anioNacimiento >= 1981 && this.anioNacimiento <= 1993) {
+      alert(
+        `${this.nombre} pertence a la GENERACION Y(millennials) y su rasgo caracteristico es la FRUSTRACION`
+      );
+    }
+    if (this.anioNacimiento >= 1994 && this.anioNacimiento <= 2010) {
+      alert(
+        `${this.nombre} pertence a la GENERACION Z y su rasgo caracteristico es la IRREVERENCIA`
+      );
+    }
+  }
 
-    mostrarDatos(){
-        alert(`>
+  esMayorDeEdad() {
+    if (this.edad >= 18) {
+      alert(`${this.nombre} es MAYOR de edad.`);
+    } else {
+      alert(`${this.nombre} es MENOR de edad.`);
+    }
+  }
+
+  mostrarDatos() {
+    alert(`>
         Nombre: ${this.nombre}
         Edad: ${this.edad}
         DNI: ${this.dni}
@@ -45,49 +54,52 @@ class Persona {
         Peso: ${this.peso}
         Altura: ${this.altura}
         Año de nacimiento: ${this.anioNacimiento}
-        `)
-    }
+        `);
+  }
 
-    generarDNI(){
-        this.dni = Math.floor(Math.random() * 100000000);
-    }
+  generarDNI() {
+    this.dni = Math.floor(Math.random() * 100000000);
+  }
 }
-
 
 let formulario = document.getElementById("formulario");
 formulario.addEventListener("submit", enviarDatos);
 
-function enviarDatos(e){
-    e.preventDefault();
-    console.log(`enviando`)
-    let nombre = document.getElementById("nombre").value;
-    let edad = document.getElementById("edad").value;
-    let sexo = document.getElementById("sexo").value;
-    let peso = document.getElementById("peso").value;
-    let altura = document.getElementById("altura").value;
-    let anio = document.getElementById("anio").value;
+function enviarDatos(e) {
+  e.preventDefault();
+  console.log(`enviando`);
+  let nombre = document.getElementById("nombre").value;
+  let edad = document.getElementById("edad").value;
+  let sexo = document.getElementById("sexo").value;
+  let peso = document.getElementById("peso").value;
+  let altura = document.getElementById("altura").value;
+  let anio = document.getElementById("anio").value;
 
-    nuevaPersona = new Persona(nombre, edad, sexo, peso, altura, anio);
-    nuevaPersona.generarDNI();
+  nuevaPersona = new Persona(nombre, edad, sexo, peso, altura, anio);
+  nuevaPersona.generarDNI();
+
+  btnMostrarInfo.disabled = false;
+  btnMostrarGen.disabled = false;
+  btnMayorEdad.disabled = false;
 }
 
 let btnMostrarInfo = document.getElementById("mostrarInfo");
 btnMostrarInfo.addEventListener("click", mostrarInfo);
 
 function mostrarInfo() {
-    nuevaPersona.mostrarDatos();
+  nuevaPersona.mostrarDatos();
 }
 
 let btnMostrarGen = document.getElementById("mostrarGen");
 btnMostrarGen.addEventListener("click", mostrarGen);
 
-function mostrarGen(){
-    nuevaPersona.mostrarGeneracion();
+function mostrarGen() {
+  nuevaPersona.mostrarGeneracion();
 }
 
 let btnMayorEdad = document.getElementById("mayor");
 btnMayorEdad.addEventListener("click", mayor);
 
-function mayor(){
-    nuevaPersona.esMayorDeEdad();
+function mayor() {
+  nuevaPersona.esMayorDeEdad();
 }
